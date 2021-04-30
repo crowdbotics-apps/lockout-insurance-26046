ffef20c1bbdd304398f2ab43fd40fe98cb9d3bb9
Crowdbotics <team@crowdbotics.com>
2021-04-30 14:00:54 +0000

    Initial commit.

---
+++ backend/lockout_insurance_26046/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lockout_insurance_26046 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lockout_insurance_26046.settings')
+
+application = get_wsgi_application()
